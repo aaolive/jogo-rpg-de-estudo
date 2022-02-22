caa5126698f7b376ff1c798eed353b5d108c0159
Anderson Oliveira <aoliveira411@gmail.com>
2022-02-22 17:09:07 -0300

    Criação do projeto inicial

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace jogo_rpg_de_estudo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
